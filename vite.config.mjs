daba6d9a278ecd8b4e06a38ec533067eb91c5d32
JSarasua <josarasua@gmail.com>
2026-01-06 16:12:36 -0600

    Attempting to get github pages working

--- vite.config.mjs
+++ vite.config.mjs
@@ -17,28 +17,31 @@ const tiledPlugin = () => {
   };
 };
 
-export default defineConfig({
-  base: "./", // optionally give a base path, useful for itch.io to serve relative instead of the default absolut
-  plugins: [tiledPlugin()], // hint vite that tiled tilesets should be treated as external
-  esbuild: {
-    supported: {
-      "top-level-await": true, //browsers can handle top-level-await features
-    },
-  },
-  // currently excalibur plugins are commonjs
-  // this forces vite to keep things from bundling ESM together with commonjs
-  optimizeDeps: {
-    exclude: ["excalibur"],
-  },
-  build: {
-    assetsInlineLimit: 0, // excalibur cannot handle inlined xml in prod mode
-    sourcemap: true,
-    // Vite uses rollup currently for prod builds so a separate config is needed
-    // to keep vite from bundling ESM together with commonjs
-    rollupOptions: {
-      output: {
-        format: "es",
+export default defineConfig(({ command }) => {
+  return {
+    // Uses the repo name for GitHub Pages, but root for local dev
+    base: command === 'build' ? '/ExcaliburTest/' : '/',
+    plugins: [tiledPlugin()], // hint vite that tiled tilesets should be treated as external
+    esbuild: {
+      supported: {
+        "top-level-await": true, //browsers can handle top-level-await features
       },
     },
-  },
+    // currently excalibur plugins are commonjs
+    // this forces vite to keep things from bundling ESM together with commonjs
+    optimizeDeps: {
+      exclude: ["excalibur"],
+    },
+    build: {
+      assetsInlineLimit: 0, // excalibur cannot handle inlined xml in prod mode
+      sourcemap: true,
+      // Vite uses rollup currently for prod builds so a separate config is needed
+      // to keep vite from bundling ESM together with commonjs
+      rollupOptions: {
+        output: {
+          format: "es",
+        },
+      },
+    },
+  };
 });
