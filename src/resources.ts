1b1a2e68f0471410da582566fcf567d4697a328a
JSarasua <josarasua@gmail.com>
2026-01-06 16:32:29 -0600

    Move resources to public folder

--- src/resources.ts
+++ src/resources.ts
@@ -3,17 +3,17 @@ import * as ex from "excalibur";
 
 export const Resources = {
 	// Relative to /public in vite
-	PipeImage: new ex.ImageSource("./images/pipe.png", {
+	PipeImage: new ex.ImageSource("/images/pipe.png", {
 		wrapping: ex.ImageWrapping.Clamp,
 	}),
-	BirdImage: new ex.ImageSource("../images/bird.png"),
-	GroundImage: new ex.ImageSource("./images/ground.png", {
+	BirdImage: new ex.ImageSource("/images/bird.png"),
+	GroundImage: new ex.ImageSource("/images/ground.png", {
 		wrapping: ex.ImageWrapping.Repeat,
 	}),
 	// Sounds
-	FlapSound: new ex.Sound("./sounds/flap.wav"),
-	FailSound: new ex.Sound("./sounds/fail.wav"),
-	ScoreSound: new ex.Sound("./sounds/score.wav"),
+	FlapSound: new ex.Sound("/sounds/flap.wav"),
+	FailSound: new ex.Sound("/sounds/fail.wav"),
+	ScoreSound: new ex.Sound("/sounds/score.wav"),
 	// Music
-	BackgroundMusic: new ex.Sound("./sounds/two_left_socks.ogg"),
+	BackgroundMusic: new ex.Sound("/sounds/two_left_socks.ogg"),
 } as const;
